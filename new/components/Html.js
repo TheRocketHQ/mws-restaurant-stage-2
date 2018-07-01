76fb23e4a5c09f246ca78171319085aa5a16a73a
Francisco Mateus <gtaskfm@gmail.com>
2018-07-01 12:54:06 +0200

    missing babel debugging

--- new/components/Html.js
+++ new/components/Html.js
@@ -1,21 +1,34 @@
-import React from 'react'; 
- 
-const Html = (props) => 
-<html> 
-<head> 
-<meta charSet="utf-8" /> 
-<meta httpEquiv="x-ua-compatible" content="ie=edge" /> 
-<title>{props.title || ''}</title> 
-<meta name="description" 
-            content={props.description || ''} /> 
-<meta name="viewport" 
-            content="width=device-width, initial-scale=1" /> 
-<script src="client.js" /> 
-</head> 
-<body> 
-<div id="app" 
-           dangerouslySetInnerHTML={{__html: props.children}} /> 
-</body> 
-</html>; 
- 
-export default Html; 
+/*
+ * Learning Isomorphic Web Application Development
+ * Copyright © 2015 Konstantin Tarkus, Packt Publishing
+ * All rights reserved.
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Html({ title, description, body }) {
+  return (
+    <html>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <script src="client.js" async />
+      </head>
+      <body>
+        <div id="app" dangerouslySetInnerHTML={{__html: body}} />
+      </body>
+    </html>
+  );
+}
+
+Html.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired
+};
+
+export default Html;
--- new/tools/run.js
+++ new/tools/run.js
@@ -1,25 +1,31 @@
+/*
+ * Learning Isomorphic Web Application Development
+ * Copyright © 2016 Konstantin Tarkus, Packt Publishing
+ */
+
 function format(time) {
-    // return human readable string 
-    return time.toTimeString() 
-                 .replace(/.*(\d{2}:\d{2}:\d{2}).*/, '$1'); 
-    } 
-    // run the async fn and time how much it took to complete 
-    async function run(fn, options) { 
-      const start = new Date(); 
-      console.log(`[${format(start)}] Starting '${fn.name}'...`); 
-      await fn(options); 
-      const end = new Date(); 
-      const time = end.getTime() - start.getTime(); 
-      console.log( 
-        `[${format(end)}] Finished '${fn.name}' after ${time} ms` 
-      ); 
-    } 
-     
-    if (process.mainModule.children.length === 0 && process.argv.length > 2) { 
-      delete require.cache[__filename]; 
-      const module = process.argv[2]; 
-      run(require('./' + module + '.js')) 
-        .catch(err => console.error(err.stack)); 
-    } 
-     
-    export default run; 
+  return time.toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, '$1');
+}
+
+function run(fn, options) {
+  const task = typeof fn.default === 'undefined' ? fn : fn.default;
+  const start = new Date();
+  console.log(
+    `[${format(start)}] Starting '${task.name}${options ? `(${options})` : ''}'...`
+  );
+  return task(options).then(() => {
+    const end = new Date();
+    const time = end.getTime() - start.getTime();
+    console.log(
+      `[${format(end)}] Finished '${task.name}${options ? `(${options})` : ''}' after ${time} ms`
+    );
+  });
+}
+
+if (process.mainModule.children.length === 0 && process.argv.length > 2) {
+  delete require.cache[__filename];
+  const module = require(`./${process.argv[2]}.js`).default;
+  run(module).catch(err => console.error(err.stack));
+}
+
+export default run;
