37fb091275cd04957523198d5deeeeb8ebcf8a91
Francisco Mateus <gtaskfm@gmail.com>
2018-07-04 21:45:55 +0200

    add js import to app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
-import './js/rainbows.js';
-import './js/unicorns.js';
+import './js/cached-files.js';
+import './js/db-helper.js';
+import './js/img-helper.js';
+import './js/main.js';
+import './js/restaurant-info.js';
+import './js/service-worker.js';
 import './scss/base.scss'
